075d2351938b74c8ae0012c33fb13c6db41a9df2
Adriano Weid <adrianoweid123@gmail.com>
2024-03-21 16:50:17 +0100

    update

--- slam.py
+++ slam.py
@@ -1,9 +1,32 @@
+#!/usr/bin/env python3
 import cv2
+import sdl2
+import sdl2.ext
+sdl2.ext.init()
 
-cap = cv2.VideoCapture("./test_countryroad.mp4")
+W = 1920//2
+H = 1080//2
 
-if not cap.isOpened():
-    print("Cannot open Video.")
+window = sdl2.ext.Window("PySDL2", (W,H))
 
-while True:
-    pass
+
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture("./test_countryroad.mp4")
+
+    if not cap.isOpened():
+        print("Cannot ope Video.")
+
+    while True:
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, (540, 380), fx=0, fy=0, interpolation=cv2.INTER_LINEAR)
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting...")
+            break
+    
+        cv2.imshow("Frame", frame)
+    
+        if cv2.waitKey(1) == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
