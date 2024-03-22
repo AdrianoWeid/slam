29e2661e70c37e7626ef7f0b93bcffcd90604452
Adriano Weid <adrianoweid123@gmail.com>
2024-03-22 11:09:06 +0100

    Video display works

--- slam.py
+++ slam.py
@@ -1,32 +1,21 @@
-#!/usr/bin/env python3
 import cv2
 import sdl2
 import sdl2.ext
+from display import Display
 sdl2.ext.init()
-
 W = 1920//2
 H = 1080//2
+display = Display(W, H)
 
-window = sdl2.ext.Window("PySDL2", (W,H))
 
-
+def process_frame(img):
+    img = cv2.resize(img, (W,H))
+    display.paint(img)    
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture("./test_countryroad.mp4")
-
-    if not cap.isOpened():
-        print("Cannot ope Video.")
-
-    while True:
+    while cap.isOpened():
         ret, frame = cap.read()
-        frame = cv2.resize(frame, (540, 380), fx=0, fy=0, interpolation=cv2.INTER_LINEAR)
-        if not ret:
-            print("Can't receive frame (stream end?). Exiting...")
-            break
-    
-        cv2.imshow("Frame", frame)
-    
-        if cv2.waitKey(1) == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
+
+        if ret == True:
+            process_frame(frame)
